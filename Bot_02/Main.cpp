35d55432c72dbc2462be11cc10a262e679a15177
SavkovAndrey <141626360+SavkovAndrey@users.noreply.github.com>
2024-05-28 10:43:11 +0300

    Добавьте файлы проекта.

---
+++ Bot_02/Main.cpp
@@ -0,0 +1,49 @@
+#include <iomanip>
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <fstream>
+#include <memory> 
+#include <vector>
+#include <list>
+#include <array>
+#include <deque>
+#include <set>
+#include <thread> 
+#include <chrono> 
+#include <algorithm>
+#include <numeric>
+#include <mutex>
+using namespace std;
+//---------------------  À¿——€ --------------------------------------------------
+
+
+//--------------------- œ–Œ“Œ“»œ€ ‘”Õ ÷»…----------------------------------------
+
+
+
+//------------------------MAIN----------------------------------------------------
+int main() {
+	setlocale(LC_ALL, "RU");
+	srand(time(NULL));
+
+
+
+
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
+
+//------------------------- –≈¿À»«¿÷»ﬂ ‘”Õ ÷»… -----------------------------------
+
+
+//------------------------- –≈¿À»«¿÷»ﬂ Ã≈“ŒƒŒ¬ (ÍÎ‡ÒÒ‡) --------------------------
+
+
